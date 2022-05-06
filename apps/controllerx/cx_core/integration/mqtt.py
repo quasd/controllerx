eb6f1a08aa7fd988d579ffe99851dfd4429b5185
eki <eki.hur@gmail.com>
2022-05-06 22:31:41 +0300

    fix syntax in ValueError

--- apps/controllerx/cx_core/integration/mqtt.py
+++ apps/controllerx/cx_core/integration/mqtt.py
@@ -30,7 +30,9 @@ class MQTTIntegration(Integration):
             try:
                 payload = json.loads(data["payload"])
             except json.decoder.JSONDecodeError:
-                raise ValueError(f"Following payload is not a valid JSON: {data["payload"]}")
+                raise ValueError(
+                    f"Following payload is not a valid JSON: {data['payload']}"
+                )
         action_key: str
         if payload_key is None:
             action_key = payload
